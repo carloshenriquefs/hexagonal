007032973e9c4da53f9307a87b81f197b558fe68
Carlos Henrique <ichfs9108@gmail.com>
2024-08-15 00:30:00 -0300

    feat: Configurando o bean

---
+++ hexagonal/src/main/kotlin/com/devdeolho/hexagonal/config/InsertCustomerConfig.kt
@@ -0,0 +1,17 @@
+package com.devdeolho.hexagonal.config
+
+import com.devdeolho.hexagonal.adapters.out.FindAddressByZipCodeAdapter
+import com.devdeolho.hexagonal.adapters.out.InsertCustomerAdapter
+import com.devdeolho.hexagonal.application.core.usecase.InsertCustomerUseCase
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class InsertCustomerConfig {
+
+    @Bean
+    fun inserCustomer(
+            findAddressByZipCodeAdapter: FindAddressByZipCodeAdapter,
+            insertCustomerAdapter: InsertCustomerAdapter
+    ) = InsertCustomerUseCase(findAddressByZipCodeAdapter, insertCustomerAdapter)
+}
