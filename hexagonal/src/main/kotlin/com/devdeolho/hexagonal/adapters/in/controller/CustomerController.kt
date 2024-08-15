671b83bd4a6ae33d5b65a0aea832ec8a0cbcf74f
Carlos Henrique <ichfs9108@gmail.com>
2024-08-15 00:16:31 -0300

    feat: Criando o controller

---
+++ hexagonal/src/main/kotlin/com/devdeolho/hexagonal/adapters/in/controller/CustomerController.kt
@@ -0,0 +1,24 @@
+package com.devdeolho.hexagonal.adapters.`in`.controller
+
+import com.devdeolho.hexagonal.adapters.`in`.controller.request.CustomerRequest
+import com.devdeolho.hexagonal.application.core.domain.Customer
+import com.devdeolho.hexagonal.application.ports.`in`.InsertCustomerInputPort
+import jakarta.validation.Valid
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/api/v1/customers")
+class CustomerController(
+        private val insertCustomerInputPort: InsertCustomerInputPort
+) {
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    fun insert(@Valid @RequestBody customerRequest: CustomerRequest) {
+        with(customerRequest) {
+            val customer = Customer(name = name, cpf = cpf)
+            insertCustomerInputPort.insert(customer, customerRequest.zipCode)
+        }
+    }
+}
