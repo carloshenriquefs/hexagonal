74d9f88a5f44569ced9d998df3214266f344846b
Carlos Henrique <ichfs9108@gmail.com>
2024-08-16 23:44:09 -0300

    feat: Criando o controller

--- hexagonal/src/main/kotlin/com/devdeolho/hexagonal/adapters/in/controller/CustomerController.kt
+++ hexagonal/src/main/kotlin/com/devdeolho/hexagonal/adapters/in/controller/CustomerController.kt
@@ -1,7 +1,9 @@
 package com.devdeolho.hexagonal.adapters.`in`.controller
 
 import com.devdeolho.hexagonal.adapters.`in`.controller.request.CustomerRequest
+import com.devdeolho.hexagonal.adapters.`in`.controller.response.CustomerResponse
 import com.devdeolho.hexagonal.application.core.domain.Customer
+import com.devdeolho.hexagonal.application.ports.`in`.FindCustomerByIdInputPort
 import com.devdeolho.hexagonal.application.ports.`in`.InsertCustomerInputPort
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
@@ -10,7 +12,8 @@ import org.springframework.web.bind.annotation.*
 @RestController
 @RequestMapping("/api/v1/customers")
 class CustomerController(
-        private val insertCustomerInputPort: InsertCustomerInputPort
+        private val insertCustomerInputPort: InsertCustomerInputPort,
+        private val findCustomerByIdInputPort: FindCustomerByIdInputPort
 ) {
 
     @PostMapping
@@ -20,5 +23,12 @@ class CustomerController(
             val customer = Customer(name = name, cpf = cpf)
             insertCustomerInputPort.insert(customer, customerRequest.zipCode)
         }
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    fun findById(@PathVariable id: String): CustomerResponse {
+        val customer = findCustomerByIdInputPort.find(id)
+        return CustomerResponse(customer)
     }
 }
