6aed12c1da643b7357006c9aad467c70d32a6e3d
Carlos Henrique <ichfs9108@gmail.com>
2024-08-16 23:55:07 -0300

    feat: Configurando o Bean

---
+++ hexagonal/src/main/kotlin/com/devdeolho/hexagonal/config/FindCustomerByIdConfig.kt
@@ -0,0 +1,14 @@
+package com.devdeolho.hexagonal.config
+
+import com.devdeolho.hexagonal.adapters.out.FindCustomerByIdAdapter
+import com.devdeolho.hexagonal.application.core.usecase.FindCustomerByIdUseCase
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class FindCustomerByIdConfig {
+
+    @Bean
+    fun findCustomerById(findCustomerByIdAdapter: FindCustomerByIdAdapter) =
+            FindCustomerByIdUseCase(findCustomerByIdAdapter)
+}
