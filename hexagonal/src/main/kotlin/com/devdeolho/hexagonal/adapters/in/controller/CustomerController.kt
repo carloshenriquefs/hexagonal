ed9483026359a6c2e3b4e619cd0e6071b4fed150
Carlos Henrique <ichfs9108@gmail.com>
2024-08-18 01:36:41 -0300

    feat: Criando o controller

--- hexagonal/src/main/kotlin/com/devdeolho/hexagonal/adapters/in/controller/CustomerController.kt
+++ hexagonal/src/main/kotlin/com/devdeolho/hexagonal/adapters/in/controller/CustomerController.kt
@@ -5,6 +5,7 @@ import com.devdeolho.hexagonal.adapters.`in`.controller.response.CustomerRespons
 import com.devdeolho.hexagonal.application.core.domain.Customer
 import com.devdeolho.hexagonal.application.ports.`in`.FindCustomerByIdInputPort
 import com.devdeolho.hexagonal.application.ports.`in`.InsertCustomerInputPort
+import com.devdeolho.hexagonal.application.ports.`in`.UpdateCustomerInputPort
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
@@ -13,7 +14,8 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("/api/v1/customers")
 class CustomerController(
         private val insertCustomerInputPort: InsertCustomerInputPort,
-        private val findCustomerByIdInputPort: FindCustomerByIdInputPort
+        private val findCustomerByIdInputPort: FindCustomerByIdInputPort,
+        private val updateCustomerInputPort: UpdateCustomerInputPort
 ) {
 
     @PostMapping
@@ -30,5 +32,14 @@ class CustomerController(
     fun findById(@PathVariable id: String): CustomerResponse {
         val customer = findCustomerByIdInputPort.find(id)
         return CustomerResponse(customer)
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun update(@PathVariable id: String, @Valid @RequestBody customerRequest: CustomerRequest) {
+        with(customerRequest) {
+            val customer = Customer(id, name, cpf = cpf)
+            updateCustomerInputPort.update(customer, customerRequest.zipCode)
+        }
     }
 }
--- hexagonal/src/main/kotlin/com/devdeolho/hexagonal/application/core/usecase/UpdateCustomerUseCase.kt
+++ hexagonal/src/main/kotlin/com/devdeolho/hexagonal/application/core/usecase/UpdateCustomerUseCase.kt
@@ -2,6 +2,7 @@ package com.devdeolho.hexagonal.application.core.usecase
 
 import com.devdeolho.hexagonal.application.core.domain.Customer
 import com.devdeolho.hexagonal.application.ports.`in`.FindCustomerByIdInputPort
+import com.devdeolho.hexagonal.application.ports.`in`.UpdateCustomerInputPort
 import com.devdeolho.hexagonal.application.ports.out.FindAddressByZipCodeOutputPort
 import com.devdeolho.hexagonal.application.ports.out.UpdateCustomerOutputPort
 
@@ -9,9 +10,9 @@ class UpdateCustomerUseCase(
         private val findCustomerByIdInputPort: FindCustomerByIdInputPort,
         private val findAddressByZipCodeOutputPort: FindAddressByZipCodeOutputPort,
         private val updateCustomerOutputPort: UpdateCustomerOutputPort
-) {
+): UpdateCustomerInputPort {
 
-    fun update(customer: Customer, zipCode: String) {
+    override fun update(customer: Customer, zipCode: String) {
         if (customer.id == null) throw IllegalArgumentException("The id field cannot be null")
         findCustomerByIdInputPort.find(customer.id)
 
