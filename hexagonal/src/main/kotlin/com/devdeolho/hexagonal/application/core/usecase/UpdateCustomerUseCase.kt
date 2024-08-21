f9c860a837d74ece6f31f0db6447527686976bf4
Carlos Henrique <ichfs9108@gmail.com>
2024-08-21 00:10:24 -0300

    feat: Enviando o CPF para validacao na alteracao

--- hexagonal/src/main/kotlin/com/devdeolho/hexagonal/application/core/usecase/UpdateCustomerUseCase.kt
+++ hexagonal/src/main/kotlin/com/devdeolho/hexagonal/application/core/usecase/UpdateCustomerUseCase.kt
@@ -4,22 +4,28 @@ import com.devdeolho.hexagonal.application.core.domain.Customer
 import com.devdeolho.hexagonal.application.ports.`in`.FindCustomerByIdInputPort
 import com.devdeolho.hexagonal.application.ports.`in`.UpdateCustomerInputPort
 import com.devdeolho.hexagonal.application.ports.out.FindAddressByZipCodeOutputPort
+import com.devdeolho.hexagonal.application.ports.out.SendCpfForValidationOutputPort
 import com.devdeolho.hexagonal.application.ports.out.UpdateCustomerOutputPort
 
 class UpdateCustomerUseCase(
         private val findCustomerByIdInputPort: FindCustomerByIdInputPort,
         private val findAddressByZipCodeOutputPort: FindAddressByZipCodeOutputPort,
-        private val updateCustomerOutputPort: UpdateCustomerOutputPort
-): UpdateCustomerInputPort {
+        private val updateCustomerOutputPort: UpdateCustomerOutputPort,
+        private val sendCpfForValidationOutputPort: SendCpfForValidationOutputPort
+) : UpdateCustomerInputPort {
 
     override fun update(customer: Customer, zipCode: String) {
         if (customer.id == null) throw IllegalArgumentException("The id field cannot be null")
-        findCustomerByIdInputPort.find(customer.id)
+        val savedCpf = findCustomerByIdInputPort.find(customer.id).cpf
+
 
         customer.apply {
             address = findAddressByZipCodeOutputPort.find(zipCode)
         }.let {
             updateCustomerOutputPort.update(it)
+            if (savedCpf != it.cpf) {
+                sendCpfForValidationOutputPort.send(it.cpf)
+            }
         }
 
     }
