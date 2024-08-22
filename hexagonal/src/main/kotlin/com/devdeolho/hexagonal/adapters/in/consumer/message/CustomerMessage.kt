c8a9f5441b546dce32947535dc76116873a5270f
Carlos Henrique <ichfs9108@gmail.com>
2024-08-22 03:30:23 -0300

    feat: Configurando o consumer

---
+++ hexagonal/src/main/kotlin/com/devdeolho/hexagonal/adapters/in/consumer/message/CustomerMessage.kt
@@ -0,0 +1,9 @@
+package com.devdeolho.hexagonal.adapters.`in`.consumer.message
+
+data class CustomerMessage(
+        val id: String,
+        val name: String,
+        val zipCode: String,
+        val cpf: String,
+        val isValidCpf: Boolean
+)
